fb9b156c0bfa973bd22e1328d9742063a5674167
xupyprblr <xupypr.blr@gmail.com>
2021-11-22 16:14:28 +0300

    plugins to stock

--- src/wp-content/plugins/google-listings-and-ads/js/build/product-attributes.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '7a364125e88593b46b95d510b8679d83');
--- src/wp-content/plugins/google-listings-and-ads/js/build/task-complete-setup.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wc-navigation', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'a50c267813e1bd5404996e85f1755801');
